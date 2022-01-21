50224901a45f7609564914ef5777110840475da8
TheJoker92 <demicco.alessandro92@gmail.com>
2022-01-21 19:57:36 +0100

    dim of V issue

---
+++ matAPILib.py
@@ -0,0 +1,148 @@
+import scipy.io
+import numpy as np
+
+class MatApi:
+
+    """
+    Given an array retrieve the number of rows
+    :param array: input vector
+    :return: returns the number of rows
+    """
+    def getNumRows(self, array):
+        rows = 1
+
+        if not(array[0].dtype == "float64"):
+            rows = len(array[0])
+
+        return rows
+
+    """
+    Given an array retrieve the number of cols
+    :param array: input vector
+    :return: returns the number of cols
+    """
+    def getNumCols(self, array):
+        return len(array)
+
+    """
+    Read the mat file
+    :param matFilePath: path of .mat file
+    :return: returns the content of .mat file
+    """
+    def readMatFile(self, matFilePath):
+        return scipy.io.loadmat(matFilePath)
+
+    """
+    Given vector V returns the length of the largest array dimension in V
+
+    :param array: input vector
+    :return: the greatest between rows and cols
+    """ 
+    def vectorLength(self, array):
+        length = 1
+
+        rows = self.getNumRows(array)
+        cols = self.getNumCols(array)
+
+        if rows > cols:
+            length = rows
+        else:
+            length = cols
+
+        return length
+
+    """
+    Create a 2-dimensional array of zeros
+
+    :param rows: number of rows
+    :param cols: number of cols
+    :return: a numpy array of size rows x cols
+    """ 
+    def zeros(self, rows, cols):
+        return np.zeros((rows, cols))
+
+    """
+    Retrieves dimension of multi-dimensional array
+
+    :param array: input vector
+    :return: a tuple of array dimensions
+    """
+    def size(self, array):
+        return array.shape
+
+    """
+    Retrieves the smallest value in the multi dimensional array
+
+    :param array: input vector
+    :return: the smallest value in the multi dimensional array
+    """
+    def min(self, array):
+        return np.amin(array)
+
+    """
+    Given two array retrieves the difference element for element
+
+    :param array1: input vector
+    :param array2: input vector
+    :return: array of the differences
+    """
+    def array_diff(self, array1, array2):
+        return np.subtract(array1, array2)
+
+    """
+    unique(array) returns the same data as in A, but with no repetitions. C is in sorted order.
+
+    :param array: input vector
+    :return: a sorted array with no repetition respect to input
+    """
+    def unique(self, array):
+        return np.unique(array)
+
+    """
+    Create a vector of n logarithmically spaced points in the interval [10^start,10^stop].
+    :param start: first value of the interval
+    :param start: last value of the interval
+    :param n: number of elements
+    :return: a sorted array with no repetition respect to input
+    """
+    def logspace(self, start, stop, n=50):
+        return np.logspace(start, stop, n)
+
+    """
+    S = sum(A) returns the sum of the elements of A along the first array dimension whose size does not equal 1.
+    :param start: first value of the interval
+    :param start: last value of the interval
+    :param n: number of elements
+    :return: a sorted array with no repetition respect to input
+    """
+    def sum(self, array):
+
+        rows = self.getNumRows(array)
+        cols = self.getNumCols(array)
+
+        count = self.zeros(rows, cols)
+
+        if rows > 1:
+            count = np.sum(array[0], axis=0)
+        elif cols > 1:
+            count = np.sum(array, axis=1)
+
+
+        return count   
+
+
+    """
+    Find indices and values of nonzero elements.
+    :param posmax: first value of the interval
+    :return: an array of index of non-zeros elements
+    """
+    def find(self, posMax):
+        return np.argwhere(posMax)
+
+    """
+    Check if array is empty:
+    :param array: input array
+    :return: True if array contains no element
+    """
+    def isempty(array):
+        return array.size == 0
--- matReader.py
+++ matReader.py
@@ -1,89 +1,7 @@
-import scipy.io
 import numpy as np
+import matAPILib
 
-class MatApi:
-
-    def readMatFile(self, matFilePath):
-        return scipy.io.loadmat(matFilePath)
-
-    """
-    Given vector V returns the length of the largest array dimension in V
-
-    :param array: input vector
-    :return: the greatest between rows and cols
-    """ 
-    def vectorLength(self, array):
-        length = 0
-        rows = len(array[0])
-        cols = len(array)
-
-        if rows > cols:
-            length = rows
-        else:
-            length = cols
-
-        return length
-
-    """
-    Create a 2-dimensional array of zeros
-
-    :param rows: number of rows
-    :param cols: number of cols
-    :return: a numpy array of size rows x cols
-    """ 
-    def zeros(self, rows, cols):
-        return np.zeros((rows, cols))
-
-    """
-    Retrieves dimension of multi-dimensional array
-
-    :param array: input vector
-    :return: a tuple of array dimensions
-    """
-    def size(self, array):
-        return array.shape
-
-    """
-    Retrieves the smallest value in the multi dimensional array
-
-    :param array: input vector
-    :return: the smallest value in the multi dimensional array
-    """
-    def min(self, array):
-        return np.amin(array)
-
-    """
-    Given two array retrieves the difference element for element
-
-    :param array1: input vector
-    :param array2: input vector
-    :return: array of the differences
-    """
-    def array_diff(self, array1, array2):
-        return np.subtract(array1, array2)
-
-    """
-    unique(array) returns the same data as in A, but with no repetitions. C is in sorted order.
-
-    :param array: input vector
-    :return: a sorted array with no repetition respect to input
-    """
-    def unique(self, array):
-        return np.unique(array)
-
-    """
-    Create a vector of n logarithmically spaced points in the interval [10^start,10^stop].
-    :param start: first value of the interval
-    :param start: last value of the interval
-    :param n: number of elements
-    :return: a sorted array with no repetition respect to input
-    """
-    def logspace(self, start, stop, n=50):
-        return np.logspace(start, stop, n)
-
-
-
-matApi = MatApi()
+matApi = matAPILib.MatApi()
 
 # readfile
 mat = matApi.readMatFile("V.mat")
@@ -95,7 +13,9 @@ display = 1
 V = matApi.array_diff(mat["V"], matApi.min(mat["V"]))
 
 
-n = matApi.vectorLength(mat["V"])
+n = matApi.vectorLength(V)
+
+print("N -> " + str(n))
 
 #definition of local variables
 buffer = matApi.zeros(n,1)
@@ -105,15 +25,98 @@ if select < 1:
 else:
     minDist = n/select
 
-#horizons = round(linspace(1,ceil(n/20),50));
+#horizons = np.round(np.linspace(1,np.ceil(n/20),50));
 horizons = matApi.unique(np.round(matApi.logspace(0,2,50)/100*np.ceil(n/20)))
 #horizons=1:2:50;
 Vorig = V
 #all this tempMat stuff is to avoid calling findpeaks which is horribly
 # slow for our purpose
 tempMat = matApi.zeros(n,3)
-tempMat[1][1]=float("inf")
+tempMat[0][0]=float("inf")
 tempMat[-1][2]=float("inf")
-print(matApi.array_diff(mat["V"], matApi.min(mat["V"])))
 
+# loop over scales
+for is_m in range(0, matApi.vectorLength(horizons)):
+    
+    # sooth data, using fft-based convolution with a half sinusoid
+    horizon = horizons[is_m]
+    print(horizon)
+    if horizon > 1:
+        w=np.max(np.eps,np.sin(2*np.pi*(range(0,horizon)))/2/(horizon-1))
+
+        w=w/matApi.sum(w);   
+        print(w) 
+        #V=.np.convolve(V,w,mode='same');
+        V = np.real(np.fft.ifft(V,n+horizon)*np.fft(w,n+horizon))
+        V = V[1+np.floor(horizon/2):np.ceil(horizon/2)]
+     
+    #   find local maxima
+    end = -1
+    tempMat[2:end] = V[1:end-1]
+    # tempMat[:,2] = V
+    # tempMat[1:end-1,3] = V[2:end]
+#     [useless,posMax] =max(tempMat,[],2)
+#     I = matApi.find(posMax)
+#     I = np.gradient(I)
+#     II = np.gradient(I)
+    
+#     #initialize buffer
+#     newbuffer = matApi.zeros(n, 1)
+    
+#     if is_m == 1:
+#         # if first iteration, keep all local maxima
+#         newbuffer = Vorig(I)
+#     else:    
+#         old = matApi.find(newbuffer)
+#         print(old)
+#         old = np.gradient(old)
+#         if matApi.isempty(old):
+#             continue
+        
+# #         %Now, for each element of I, find the closest element in
+# #         %old along with its distance. The few nice lines below were
+# #         %written by Roger Stafford in a forum post available here:
+# #         %http://www.mathworks.fr/matlabcentral/newsreader/view_thread/24387
+#         [c,p] = np.sort(old)
+#         #C = np.gradient(c);
+#         [useless,ic] = np.histc(I,[-float("inf"),(c[1:-2]+c[2:-1])/2,float("inf")])
+#         iOld = p(ic)
+#         d = abs(I-old(iOld));
+#         %D = d';
+ 
+#         %done, now select only those that are sufficiently close
+#         neighbours = iOld(d<minDist);
+#         if ~isempty(neighbours)
+#             B=V(old(neighbours))*is^2;
+#             newBuffer(old(neighbours)) = B;
+#         end
+#     end
+#     %update stuff
+#     buffer = newBuffer;
+#     criterion = criterion + newBuffer;
+# end
+# %normalize criterion
+# criterion = criterion/max(criterion);
+# %find peaks based on criterion
+# if select<1
+#     peaks = find(criterion>select);
+# else
+# %     sorted = find(criterion>1E-3);
+# %     [~,order] = sort(criterion(sorted),'descend');
+# %     peaks = sorted(order(1:min(length(sorted),select)));
+#     [a,order] = sort(criterion,'descend');
+#     peaks = order(1:select);
+# end
+# if display
+#     %display
+#     clf
+#     plot(Vorig,'LineWidth',2);
+#     hold on
+#     plot(criterion*max(Vorig),'r');
+#     hold on
+#     plot(peaks,Vorig(peaks),'ro','MarkerSize',10,'LineWidth',2)
+#     grid on
+#     title('Scale-space peak detection','FontSize',16);
+#     legend('data','computed criterion','selected peaks');
+# end
 
